f878f0006d2c90a62b040a93593e383b05ae0f61
coyotespike <timothy.roy@protonmail.ch>
2023-06-03 17:56:36 -0500

    Add workflow to publish to PyPi

--- setup.py
+++ setup.py
@@ -1,2 +1,23 @@
 from setuptools import setup
-setup(name="voyager_dev", version="0.1", packages=["voyager_dev"], entry_points={"console_scripts": ["voyager_dev=voyager_dev.voyager_dev:main"]})
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read().split('# Contributions and Issues')[0]
+
+setup(
+    name='voyager_dev',
+    version='0.0.1',
+    author='Timothy Roy',
+    author_email='tim@gradientflow.ai',
+    description='A package for coding assistance',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/coyotespike/voyager_dev',
+    packages=['voyager_dev'],
+    entry_points={'console_scripts': ['voyager_dev=voyager_dev.voyager_dev:main']},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
